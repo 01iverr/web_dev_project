951d25c589d51f3fa1642a376e6fd3c4a0beeac1
anape03 <anapetr@hotmail.com>
2022-12-26 19:35:20 +0200

    loads categories in index page In index.js (app.get('/', (req,res)=> {...});) it fetches categories, and using main.handlebars (header-footer), and categories.handlebars (main content for page) it creates the html file dynamically.
    Not used:
    * public/javascipt
    * public/html
    
    TODO:
    * Fix appearance for categories.handlebars (index page)
    * In same page: images must link to corresponding product pages
    * Make category.html
    * Fetch info for subcategories

--- index.js
+++ index.js
@@ -1,36 +1,79 @@
-const cors =require ('cors');
+const cors = require ('cors');
 const express = require('express');
 const path = require('path');
+const handlebars = require('express-handlebars');
 const app = express();
 const port = 8080;
 
-const corsOptions = {
-  origin: "https://wiki-shop.onrender.com/categories/",
-  originSuccessStatus: 200,
-};
-app.listen(port)
+app.engine(
+    "handlebars",
+    handlebars.engine({
+      layoutsDir: path.join(__dirname, "views", "layouts"),
+      defaultLayout: "main",
+    })
+  );
+  app.set("view engine", "handlebars");
+  app.set("views", path.join(__dirname, "views"));
 
 /*
     Serve static content from directory "public",
     it will be accessible under path /,
     e.g. http://localhost:8080/index.html
 */
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 // parse url-encoded content from body
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
 
 // parse application/json content from body
-app.use(express.json())
+app.use(express.json());
+
+// use corse with headers
+app.use(cors(origin="https://wiki-shop.onrender.com/"));
 
 // serve index.html as content root
-app.get('/',cors(corsOptions), function(req, res){
+app.get('/', (req, res) => {
 
-    var options = {
-        root: path.join(__dirname, 'public')
+    // var options = {
+    //     root: path.join(__dirname, 'public')
+    // }
+
+    // res.sendFile('html/index.html', options, function(err){
+    //     console.log(err)
+    // })
+
+
+    let myHeaders = new Headers();
+    myHeaders.append('Accept', 'application/json');
+
+    let init = {
+        method: "GET",
+        headers: myHeaders
     }
 
-    res.sendFile('html/index.html', options, function(err){
-        console.log(err)
-    })
-})
+    const url = "https://wiki-shop.onrender.com/categories/";
+
+    fetch(url, init)
+        .then(response => {
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                console.log("[!] Something went wrong [!]");
+            }
+        })
+        .then((data) => {
+            res.render("categories", { 
+                page_title: "Product Categories",
+                categories: data 
+            });
+            console.log("[ Rendered categories page. ]")
+        })
+        .catch((err) => {
+			console.log(err);
+		});
+
+    // res.render("categories");
+    // console.log("[ Rendered categories page. ]")
+});
+
+app.listen(port, () => console.log('App listening to port',port));
--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,111 +1,34 @@
-const url = "https://wiki-shop.onrender.com/categories/";
-window.onload = init;
-var templates={};
-var categorieslength;
+// const url = "https://wiki-shop.onrender.com/categories/";
+// window.onload = init;
 
-function init() {
-  var categ_div = document.querySelector('#categorieslist');
-  makeCategoriesRequest(url, categ_div);
+// function init() {
+//     makeCategoriesRequest();
+// }
 
-}
+// function makeCategoriesRequest() {
+//     let myHeaders = new Headers();
+//     myHeaders.append('Accept', 'application/json');
 
-function makeCategoriesRequest(url, div) {
-  let myHeaders = new Headers();
-  myHeaders.append('Access-Control-Allow-Origin' , '*');
-  myHeaders.append('Accept', 'application/json');
+//     let init = {
+//         method: "GET",
+//         headers: myHeaders
+//     }
 
-  let init = {
-    method: "GET",
-    headers: myHeaders
-  }
-
-  fetch(url, init)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        console.log(">!< Looks like something went wrong :/ >!<");
-      }
-    })
-
-    .then(data => {
-      categorieslength = Object.keys(data).length;
-      let categoryData = {
-        "categories": [],
-        "subcategories": []
-      }
-      for (category of data) {
-        let categoryItem = {
-          "id": category.id,
-          "title": category.title,
-          "img_url": category.img_url,
-        }
-        let subdata = makeCategoriesRequest(url + category.id + "/subcategories");
-        categoryData.categories.push(categoryItem);
-        categoryData.subcategories.push(subdata);
-
-      }
-      let ctgrContent = templates.categories(categoryData);
-      div.innerHTML = ctgrContent;
-              console.log(categoryData);
-    })
-    .catch(error => {
-      console.log(">!< Fetch error >!<", error);
-    })
-}
-
-function makeSubCategoriesRequest(url) {
-  let categoryData;
-  let myHeaders = new Headers();
-  myHeaders.append('Accept', 'application/json');
-  myHeaders.append('Access-Control-Allow-Origin' , '*');
-  let init = {
-    method: "GET",
-    headers: myHeaders
-  }
-  fetch(url, init)
-    .then(response => {
-      if (response.status == 200) {
-        return response.json();
-      } else {
-        console.log(">!< Looks like something went wrong :/ >!<");
-      }
-    })
-    .then(data => {
-      categoryData = {
-        "categories": []
-      }
-      for (category of data) {
-        let categoryItem = {
-          "id": category.id,
-          "title": category.title,
-          "category_id": category.subcategory_id,
-        }
-        categoryData.categories.push(categoryItem)
-      }
-    })
-    .catch(error => {
-      console.log(">!< Fetch error >!<", error);
-    })
-  // return categoryData;
-}
-
-templates.categories = Handlebars.compile(`
-  <li>
-    {{title}}
-    <img src="{{img_url}}" alt="photo of cat">
-    {{#if children}}
-      <ul>
-        {{#each children}}
-        {{#ifCond {{id}} {{subcategory_id}} }}
-            {{v1}} is equal to {{v2}}
-              <li>
-                  {{subname}}
-              </li>
-        {{/ifCond}}
-
-        {{/each}}
-      </ul>
-    {{/if}}
-  </li>
-`);
+//     fetch(url, init)
+//         .then(response => {
+//             if (response.status == 200) {
+//                 return response.json();
+//             } else {
+//                 console.log("[!] Something went wrong [!]");
+//             }
+//         })
+//         .then((data) => {
+//             res.render("categories", { 
+//                 categories: data 
+//             });
+//             console.log("[ Rendered categories page. ]")
+//         })
+//         .catch((err) => {
+// 			console.log(err);
+// 		});
+// }
